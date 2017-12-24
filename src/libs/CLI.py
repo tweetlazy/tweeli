f84e926a0a283fc8afd0e84a4e4a9697648c1098
m.taheri <m.taheri@parspooyesh.com>
2017-12-24 17:08:20 +0330

    add auto complete to show command

--- src/libs/CLI.py
+++ src/libs/CLI.py
@@ -7,8 +7,8 @@ from .CLI_Show import TwitterCLIShow
 
 class TwitterCLI(Cmd):
 
-    def preloop(self):
-        Cmd.preloop(self)
+    SHOW_USERS = ['me', 'user']
+    SHOW_COMMANDS = ['timeline', 'follower', 'following', 'friend', 'noback', 'notbacked', 'home']
 
     def start(self):
         self.__twitterCore = TwitterCore()
@@ -17,47 +17,63 @@ class TwitterCLI(Cmd):
         self.prompt='Twitter>>> '
 
     def do_show(self, line):
-        params = line.split()
-        if params[0] == 'me':
-            if len(params) == 1:
-               self.__CLIShow.displayOwnerAccount()
-            else:
-                command = params[1]
-                if command == 'timeline':
-                   self.__CLIShow.displayOwnerTimeline()
-                elif command == 'follower':
-                   self.__CLIShow.displayOwnerFollowers()
-                elif command == 'following':
-                   self.__CLIShow.displayOwnerFollowings()
-                elif command == 'friend':
-                   self.__CLIShow.displayOwnerFriends()
-                elif command == 'noback':
-                   self.__CLIShow.displayOwnerNoBack()
-                elif command == 'notbacked':
-                   self.__CLIShow.displayOwnerNotBacked()
-                elif command == 'home':
-                   self.__CLIShow.displayOwnerHome()
-        elif params[0] == 'user':
-            if len(params) == 2:
-                userName = params[1]
-                self.__CLIShow.displayAccount(userName)
-            else:
-                userName = params[1]
-                command = params[2]
-                if command == 'timeline':
-                   self.__CLIShow.displayTimeline(userName)
-                elif command == 'follower':
-                   self.__CLIShow.displayFollowers(userName)
-                elif command == 'following':
-                   self.__CLIShow.displayFollowings(userName)
-                elif command == 'friend':
-                   self.__CLIShow.displayFriends(userName)
-                elif command == 'noback':
-                   self.__CLIShow.displayNoBack(userName)
-                elif command == 'notbacked':
-                   self.__CLIShow.displayNotBacked(userName)
-        elif params[0] == 'help':
-            self.__CLIShow.displayHelp()
+        if line == 'me':
+            self.__CLIShow.displayOwnerAccount()
+        elif line == 'me timeline':
+            self.__CLIShow.displayOwnerTimeline()
+        elif line == 'me follower':
+            self.__CLIShow.displayOwnerFollowers()
+        elif line == 'me following':
+            self.__CLIShow.displayOwnerFollowings()
+        elif line == 'me friend':
+            self.__CLIShow.displayOwnerFriends()
+        elif line == 'me noback':
+            self.__CLIShow.displayOwnerNoBack()
+        elif line == 'me notbacked':
+            self.__CLIShow.displayOwnerNotBacked()
+        elif line == 'me home':
+            self.__CLIShow.displayOwnerHome()
+        elif 'user ' and ' timeline' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayTimeline(userName)
+        elif 'user ' and ' follower' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayFollowers(userName)
+        elif 'user ' and ' following' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayFollowings(userName)
+        elif 'user ' and ' friend' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayFriends(userName)
+        elif 'user ' and ' noback' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayNoBack(userName)
+        elif 'user ' and ' notbacked' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayNotBacked(userName)
+        elif 'user ' in line:
+            userName = line.split()[1]
+            self.__CLIShow.displayAccount(userName)
+        else:
+            print("*** Unknown syntax: %s"%line)
+
+    def complete_show(self, text, line, begidx, endidx):
+        commands = line.split()
+        if not text:
+            if len(commands) == 1:
+                completions = self.SHOW_USERS[:]
+            elif len(commands) == 2 and 'me' in line:
+                completions = self.SHOW_COMMANDS[:]
+            elif len(commands) == 3:
+                completions = self.SHOW_COMMANDS[:]
+        else:
+            if len(commands) == 2:
+                completions = [f for f in self.SHOW_USERS if f.startswith(text)]
+            elif len(commands) == 3 and 'me' in line:
+                completions = self.SHOW_COMMANDS[:]
+            elif len(commands) == 4:
+                completions = [f for f in self.SHOW_COMMANDS if f.startswith(text)]
+        return completions
 
     def do_follow(self, line):
         userName = line
