a668b441bcd3cad14370daa5571c30c1b6292b56
m.j_banitaba <m.j_banitaba@iran.ir>
2018-07-12 22:22:44 +0430

    login and logout

--- src/libs/API.py
+++ src/libs/API.py
@@ -1,45 +1,41 @@
-
 import uuid, json, tweepy
-from flask import Flask, request, session, send_from_directory
-from .Core import TwitterCore
+from flask import Flask, request, session, send_from_directory,redirect
+# from .Core import TwitterCore
 
 app = Flask(__name__)
 
 objectStorage = {}
 
 def isLoggedIn():
-    if 'sessionID' in session:
+    app.logger.debug("login state : %s",str('twt_user' in session))
+    if 'twt_user' in session:
         return True
     else:
         return False
 
-def login(params):
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    session.clear()
+    return redirect("/html/login.html",code=302)
+@app.route("/login", methods=['POST'])
+def login():
+    session['twt_user']=request.form.to_dict()
     if isLoggedIn():
-        errorMsg = {'status_code':'4401', 'description':'You are already logged in!'}
-        return json.dumps(errorMsg)
-    else:
-        action = "login"
-        twitterCore = TwitterCore()
-        sessionID = uuid.uuid1()
-        session['sessionID'] = sessionID
-        objectStorage[sessionID] = twitterCore
-        method = getattr(twitterCore, action)
-        try:
-            method(**params)
-            response = {'status_code':'2200', 'description':'Login successfully!'}
-            return json.dumps(response)
-        except:
-            response = {'status_code':'4500', 'description':'Internal error!'}
-            return json.dumps(response)
-
+        return redirect("/html/index.html",code=302)
+    return '';
 @app.route('/html/<path:path>')
 def send_js(path):
+    if (path != "login.html")and ('html' in path):
+        if not isLoggedIn():
+            return redirect("/html/login.html",code=302)
+    if (path == "login.html") and isLoggedIn():
+        return redirect("/html/index.html",code=302)
     return send_from_directory('html', path)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    welcomeMsg = "Welcome to Twitter App!"
-    return welcomeMsg
+
+    return redirect("/html/index.html",code=302)
 
 @app.route("/api", methods=['GET', 'POST'])
 def api():
@@ -85,7 +81,7 @@ def runServer():
     # log = logging.getLogger('werkzeug')
     # log.disabled = True
     app.secret_key = 'lvkjlfLJLJEIOFs;ffiojsfjelsk'
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
 
 if __name__ == "__main__":
     runServer()
